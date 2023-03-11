ee5ebfb0867c9e0a663cc3e7008606c5ff7de052
szm8991 <songzheming157@163.com>
2023-03-11 10:10:08 +0800

    chore: add build source map

---
+++ packages/compiler/build.config.ts
@@ -0,0 +1,13 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: ['packages/vue/src/index'],
+  declaration: true, // generate .d.ts files
+  clean: true,
+  rollup: {
+    emitCJS: true,
+    esbuild: {
+      sourceMap: true,
+    },
+  },
+})
